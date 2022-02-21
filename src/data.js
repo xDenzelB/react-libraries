b07ea5181be5c2f399c9b55494d830cfa5000dde
Denzel <dbartolaba123@gmail.com>
2022-02-21 15:21:01 -0800

    Added in data

---
+++ src/data.js
@@ -0,0 +1,602 @@
+export default 
+[{
+  'id': 1,
+  'first_name': 'Madel',
+  'last_name': 'Kendle',
+  'email': 'mkendle0@wisc.edu',
+  'gender': 'Male',
+  'company name ': 'Zoomlounge',
+  'car_make': 'Mercury',
+  'car_year': 2005,
+  'year_born': '1991'
+}, {
+  'id': 2,
+  'first_name': 'Tamra',
+  'last_name': 'Espinha',
+  'email': 'tespinha1@com.com',
+  'gender': 'Female',
+  'company name ': 'Tanoodle',
+  'car_make': 'Chevrolet',
+  'car_year': 1992,
+  'year_born': '1991'
+}, {
+  'id': 3,
+  'first_name': 'Melody',
+  'last_name': 'Camocke',
+  'email': 'mcamocke2@craigslist.org',
+  'gender': 'Male',
+  'company name ': 'Edgewire',
+  'car_make': 'Lexus',
+  'car_year': 2011,
+  'year_born': '2004'
+}, {
+  'id': 4,
+  'first_name': 'Joleen',
+  'last_name': 'Willerton',
+  'email': 'jwillerton3@freewebs.com',
+  'gender': 'Female',
+  'company name ': 'Voonder',
+  'car_make': 'Suzuki',
+  'car_year': 1994,
+  'year_born': '1998'
+}, {
+  'id': 5,
+  'first_name': 'Kelcie',
+  'last_name': 'Pena',
+  'email': 'kpena4@naver.com',
+  'gender': 'Male',
+  'company name ': 'Voonder',
+  'car_make': 'Honda',
+  'car_year': 2001,
+  'year_born': '1991'
+}, {
+  'id': 6,
+  'first_name': 'Skip',
+  'last_name': 'Ondracek',
+  'email': 'sondracek5@skype.com',
+  'gender': 'Male',
+  'company name ': 'Yoveo',
+  'car_make': 'Chevrolet',
+  'car_year': 1999,
+  'year_born': '2001'
+}, {
+  'id': 7,
+  'first_name': 'Conni',
+  'last_name': 'Caustic',
+  'email': 'ccaustic6@cloudflare.com',
+  'gender': 'Female',
+  'company name ': 'Topicware',
+  'car_make': 'Buick',
+  'car_year': 1991,
+  'year_born': '2001'
+}, {
+  'id': 8,
+  'first_name': 'Warren',
+  'last_name': 'Eadmead',
+  'email': 'weadmead7@geocities.jp',
+  'gender': 'Female',
+  'company name ': 'Gigazoom',
+  'car_make': 'GMC',
+  'car_year': 2000,
+  'year_born': '2002. 1996'
+}, {
+  'id': 9,
+  'first_name': 'Harley',
+  'last_name': 'Debenham',
+  'email': 'hdebenham8@go.com',
+  'gender': 'Male',
+  'company name ': 'Photospace',
+  'car_make': 'GMC',
+  'car_year': 1999,
+  'year_born': '1970'
+}, {
+  'id': 10,
+  'first_name': 'Tadeo',
+  'last_name': 'Leverage',
+  'email': 'tleverage9@privacy.gov.au',
+  'gender': 'Female',
+  'company name ': 'Feedbug',
+  'car_make': 'Honda',
+  'car_year': 1997,
+  'year_born': '2004'
+}, {
+  'id': 11,
+  'first_name': 'Eugenia',
+  'last_name': 'Yewdale',
+  'email': 'eyewdalea@artisteer.com',
+  'gender': 'Male',
+  'company name ': 'Oloo',
+  'car_make': 'Dodge',
+  'car_year': 1994,
+  'year_born': '1970'
+}, {
+  'id': 12,
+  'first_name': 'Timi',
+  'last_name': 'Airlie',
+  'email': 'tairlieb@jugem.jp',
+  'gender': 'Bigender',
+  'company name ': 'Jaxworks',
+  'car_make': 'GMC',
+  'car_year': 1998,
+  'year_born': '1991'
+}, {
+  'id': 13,
+  'first_name': 'Kevyn',
+  'last_name': 'Jonczyk',
+  'email': 'kjonczykc@parallels.com',
+  'gender': 'Female',
+  'company name ': 'Bluejam',
+  'car_make': 'Volkswagen',
+  'car_year': 2009,
+  'year_born': '1991'
+}, {
+  'id': 14,
+  'first_name': 'Karel',
+  'last_name': 'Sidsaff',
+  'email': 'ksidsaffd@home.pl',
+  'gender': 'Female',
+  'company name ': 'Oyonder',
+  'car_make': 'Audi',
+  'car_year': 1985,
+  'year_born': '1989'
+}, {
+  'id': 15,
+  'first_name': 'Marja',
+  'last_name': 'Surgenor',
+  'email': 'msurgenore@histats.com',
+  'gender': 'Male',
+  'company name ': 'Tagopia',
+  'car_make': 'Saturn',
+  'car_year': 1998,
+  'year_born': '1998'
+}, {
+  'id': 16,
+  'first_name': 'Moe',
+  'last_name': 'Chiddy',
+  'email': 'mchiddyf@hostgator.com',
+  'gender': 'Female',
+  'company name ': 'Topicstorm',
+  'car_make': 'Pontiac',
+  'car_year': 1986,
+  'year_born': '2002. 1996'
+}, {
+  'id': 17,
+  'first_name': 'Ichabod',
+  'last_name': 'Horrell',
+  'email': 'ihorrellg@posterous.com',
+  'gender': 'Female',
+  'company name ': 'Skynoodle',
+  'car_make': 'Audi',
+  'car_year': 2006,
+  'year_born': '2002. 1996'
+}, {
+  'id': 18,
+  'first_name': 'Fredericka',
+  'last_name': 'Skate',
+  'email': 'fskateh@marketwatch.com',
+  'gender': 'Female',
+  'company name ': 'Quamba',
+  'car_make': 'Mitsubishi',
+  'car_year': 2001,
+  'year_born': '1993'
+}, {
+  'id': 19,
+  'first_name': 'Winny',
+  'last_name': 'Langshaw',
+  'email': 'wlangshawi@google.com.br',
+  'gender': 'Female',
+  'company name ': 'Gabtype',
+  'car_make': 'Buick',
+  'car_year': 1986,
+  'year_born': '2001'
+}, {
+  'id': 20,
+  'first_name': 'Pattin',
+  'last_name': "O'Nions",
+  'email': 'ponionsj@paginegialle.it',
+  'gender': 'Male',
+  'company name ': 'Camimbo',
+  'car_make': 'Ford',
+  'car_year': 2000,
+  'year_born': '1989'
+}, {
+  'id': 21,
+  'first_name': 'Pebrook',
+  'last_name': 'Symones',
+  'email': 'psymonesk@ifeng.com',
+  'gender': 'Genderfluid',
+  'company name ': 'Kimia',
+  'car_make': 'GMC',
+  'car_year': 2002,
+  'year_born': '1997'
+}, {
+  'id': 22,
+  'first_name': 'Jephthah',
+  'last_name': 'Forster',
+  'email': 'jforsterl@nifty.com',
+  'gender': 'Female',
+  'company name ': 'Yacero',
+  'car_make': 'BMW',
+  'car_year': 2007,
+  'year_born': '1992'
+}, {
+  'id': 23,
+  'first_name': 'Ferne',
+  'last_name': 'Chazier',
+  'email': 'fchazierm@blog.com',
+  'gender': 'Genderfluid',
+  'company name ': 'Zazio',
+  'car_make': 'Chevrolet',
+  'car_year': 2004,
+  'year_born': '1991'
+}, {
+  'id': 24,
+  'first_name': 'Field',
+  'last_name': 'Silcox',
+  'email': 'fsilcoxn@netvibes.com',
+  'gender': 'Female',
+  'company name ': 'Janyx',
+  'car_make': 'Eagle',
+  'car_year': 1993,
+  'year_born': '1998'
+}, {
+  'id': 25,
+  'first_name': 'Mella',
+  'last_name': 'Gumey',
+  'email': 'mgumeyo@de.vu',
+  'gender': 'Female',
+  'company name ': 'Tekfly',
+  'car_make': 'Buick',
+  'car_year': 1987,
+  'year_born': '1989'
+}, {
+  'id': 26,
+  'first_name': 'Neddie',
+  'last_name': 'Petschelt',
+  'email': 'npetscheltp@yandex.ru',
+  'gender': 'Male',
+  'company name ': 'Demizz',
+  'car_make': 'Aston Martin',
+  'car_year': 2012,
+  'year_born': '1989'
+}, {
+  'id': 27,
+  'first_name': 'Joanna',
+  'last_name': 'Burgwyn',
+  'email': 'jburgwynq@51.la',
+  'gender': 'Female',
+  'company name ': 'Trilia',
+  'car_make': 'Mitsubishi',
+  'car_year': 2002,
+  'year_born': '2002. 1996'
+}, {
+  'id': 28,
+  'first_name': 'Manda',
+  'last_name': 'Roj',
+  'email': 'mrojr@alibaba.com',
+  'gender': 'Female',
+  'company name ': 'Jabbertype',
+  'car_make': 'Toyota',
+  'car_year': 2012,
+  'year_born': '2004'
+}, {
+  'id': 29,
+  'first_name': 'Tootsie',
+  'last_name': 'Izaks',
+  'email': 'tizakss@trellian.com',
+  'gender': 'Female',
+  'company name ': 'Twimm',
+  'car_make': 'Pontiac',
+  'car_year': 1985,
+  'year_born': '2002. 1996'
+}, {
+  'id': 30,
+  'first_name': 'Tawnya',
+  'last_name': 'Joscelyne',
+  'email': 'tjoscelynet@amazon.co.jp',
+  'gender': 'Male',
+  'company name ': 'Yotz',
+  'car_make': 'Ford',
+  'car_year': 1990,
+  'year_born': '1993'
+}, {
+  'id': 31,
+  'first_name': 'Nicolis',
+  'last_name': 'Ferro',
+  'email': 'nferrou@simplemachines.org',
+  'gender': 'Male',
+  'company name ': 'Wikivu',
+  'car_make': 'Honda',
+  'car_year': 1996,
+  'year_born': '2004'
+}, {
+  'id': 32,
+  'first_name': 'Winifred',
+  'last_name': 'Horstead',
+  'email': 'whorsteadv@unesco.org',
+  'gender': 'Male',
+  'company name ': 'Tagchat',
+  'car_make': 'Volkswagen',
+  'car_year': 1989,
+  'year_born': '1993'
+}, {
+  'id': 33,
+  'first_name': 'Gustave',
+  'last_name': 'Seyffert',
+  'email': 'gseyffertw@sitemeter.com',
+  'gender': 'Female',
+  'company name ': 'Wordpedia',
+  'car_make': 'Chevrolet',
+  'car_year': 2012,
+  'year_born': '1999'
+}, {
+  'id': 34,
+  'first_name': 'Fedora',
+  'last_name': 'Shutte',
+  'email': 'fshuttex@google.cn',
+  'gender': 'Male',
+  'company name ': 'Edgepulse',
+  'car_make': 'Hyundai',
+  'car_year': 1998,
+  'year_born': '1999'
+}, {
+  'id': 35,
+  'first_name': 'Ancell',
+  'last_name': 'Levin',
+  'email': 'aleviny@i2i.jp',
+  'gender': 'Female',
+  'company name ': 'Vinder',
+  'car_make': 'Chevrolet',
+  'car_year': 2000,
+  'year_born': '1993'
+}, {
+  'id': 36,
+  'first_name': 'Susy',
+  'last_name': 'Cess',
+  'email': 'scessz@hud.gov',
+  'gender': 'Female',
+  'company name ': 'Innotype',
+  'car_make': 'Jaguar',
+  'car_year': 2001,
+  'year_born': '1991'
+}, {
+  'id': 37,
+  'first_name': 'Dannel',
+  'last_name': 'Father',
+  'email': 'dfather10@ning.com',
+  'gender': 'Male',
+  'company name ': 'Yacero',
+  'car_make': 'Subaru',
+  'car_year': 2012,
+  'year_born': '1970'
+}, {
+  'id': 38,
+  'first_name': 'Konstanze',
+  'last_name': 'Ashall',
+  'email': 'kashall11@sakura.ne.jp',
+  'gender': 'Female',
+  'company name ': 'Kwideo',
+  'car_make': 'Chevrolet',
+  'car_year': 2009,
+  'year_born': '2001'
+}, {
+  'id': 39,
+  'first_name': 'Omero',
+  'last_name': 'Carillo',
+  'email': 'ocarillo12@miibeian.gov.cn',
+  'gender': 'Male',
+  'company name ': 'Feedfish',
+  'car_make': 'Dodge',
+  'car_year': 1985,
+  'year_born': '1992'
+}, {
+  'id': 40,
+  'first_name': 'Ron',
+  'last_name': 'Thunder',
+  'email': 'rthunder13@biblegateway.com',
+  'gender': 'Male',
+  'company name ': 'Jetpulse',
+  'car_make': 'Rolls-Royce',
+  'car_year': 2011,
+  'year_born': '1970'
+}, {
+  'id': 41,
+  'first_name': 'Kaye',
+  'last_name': 'Mercer',
+  'email': 'kmercer14@arizona.edu',
+  'gender': 'Male',
+  'company name ': 'Fadeo',
+  'car_make': 'Dodge',
+  'car_year': 1995,
+  'year_born': '1990'
+}, {
+  'id': 42,
+  'first_name': 'Letisha',
+  'last_name': 'Schmidt',
+  'email': 'lschmidt15@creativecommons.org',
+  'gender': 'Female',
+  'company name ': 'Thoughtsphere',
+  'car_make': 'Dodge',
+  'car_year': 1992,
+  'year_born': '1989'
+}, {
+  'id': 43,
+  'first_name': 'Priscilla',
+  'last_name': 'Strotton',
+  'email': 'pstrotton16@vistaprint.com',
+  'gender': 'Female',
+  'company name ': 'Agivu',
+  'car_make': 'Ford',
+  'car_year': 1999,
+  'year_born': '2001'
+}, {
+  'id': 44,
+  'first_name': 'Shayla',
+  'last_name': 'Dall',
+  'email': 'sdall17@nytimes.com',
+  'gender': 'Male',
+  'company name ': 'Zoombeat',
+  'car_make': 'Dodge',
+  'car_year': 1998,
+  'year_born': '1990'
+}, {
+  'id': 45,
+  'first_name': 'Lisbeth',
+  'last_name': 'Stanyforth',
+  'email': 'lstanyforth18@plala.or.jp',
+  'gender': 'Male',
+  'company name ': 'Feedspan',
+  'car_make': 'Ford',
+  'car_year': 2001,
+  'year_born': '1993'
+}, {
+  'id': 46,
+  'first_name': 'Marguerite',
+  'last_name': 'Tylor',
+  'email': 'mtylor19@answers.com',
+  'gender': 'Male',
+  'company name ': 'Twitterlist',
+  'car_make': 'Maserati',
+  'car_year': 2004,
+  'year_born': '2004'
+}, {
+  'id': 47,
+  'first_name': 'Hobey',
+  'last_name': 'Monks',
+  'email': 'hmonks1a@illinois.edu',
+  'gender': 'Male',
+  'company name ': 'Wordpedia',
+  'car_make': 'Mercedes-Benz',
+  'car_year': 2008,
+  'year_born': '2002. 1996'
+}, {
+  'id': 48,
+  'first_name': 'Emyle',
+  'last_name': 'Sultana',
+  'email': 'esultana1b@tmall.com',
+  'gender': 'Female',
+  'company name ': 'Livetube',
+  'car_make': 'Nissan',
+  'car_year': 2005,
+  'year_born': '1993'
+}, {
+  'id': 49,
+  'first_name': 'Hazlett',
+  'last_name': 'Costard',
+  'email': 'hcostard1c@squarespace.com',
+  'gender': 'Female',
+  'company name ': 'Fivechat',
+  'car_make': 'Ford',
+  'car_year': 1992,
+  'year_born': '1997'
+}, {
+  'id': 50,
+  'first_name': 'Cos',
+  'last_name': 'Rany',
+  'email': 'crany1d@unc.edu',
+  'gender': 'Female',
+  'company name ': 'Wikizz',
+  'car_make': 'Chevrolet',
+  'car_year': 1999,
+  'year_born': '1992'
+}, {
+  'id': 51,
+  'first_name': 'Nancey',
+  'last_name': 'Marusic',
+  'email': 'nmarusic1e@cbc.ca',
+  'gender': 'Non-binary',
+  'company name ': 'Livepath',
+  'car_make': 'Kia',
+  'car_year': 2006,
+  'year_born': '1997'
+}, {
+  'id': 52,
+  'first_name': 'Pincus',
+  'last_name': 'Prettyjohns',
+  'email': 'pprettyjohns1f@blinklist.com',
+  'gender': 'Female',
+  'company name ': 'Babbleset',
+  'car_make': 'Lexus',
+  'car_year': 2008,
+  'year_born': '1998'
+}, {
+  'id': 53,
+  'first_name': 'Margareta',
+  'last_name': 'Mallabon',
+  'email': 'mmallabon1g@w3.org',
+  'gender': 'Male',
+  'company name ': 'Meejo',
+  'car_make': 'Kia',
+  'car_year': 2008,
+  'year_born': '1970'
+}, {
+  'id': 54,
+  'first_name': 'Margaretta',
+  'last_name': 'Aylin',
+  'email': 'maylin1h@macromedia.com',
+  'gender': 'Female',
+  'company name ': 'Wikido',
+  'car_make': 'Mercedes-Benz',
+  'car_year': 1998,
+  'year_born': '2004'
+}, {
+  'id': 55,
+  'first_name': 'Fleurette',
+  'last_name': 'Ashurst',
+  'email': 'fashurst1i@mapquest.com',
+  'gender': 'Male',
+  'company name ': 'Zoonoodle',
+  'car_make': 'Mercedes-Benz',
+  'car_year': 2008,
+  'year_born': '1990'
+}, {
+  'id': 56,
+  'first_name': 'Dari',
+  'last_name': 'Mongenot',
+  'email': 'dmongenot1j@nymag.com',
+  'gender': 'Male',
+  'company name ': 'Tazz',
+  'car_make': 'Toyota',
+  'car_year': 2008,
+  'year_born': '1970'
+}, {
+  'id': 57,
+  'first_name': 'Teriann',
+  'last_name': 'Jurges',
+  'email': 'tjurges1k@yahoo.co.jp',
+  'gender': 'Female',
+  'company name ': 'Realpoint',
+  'car_make': 'Mitsubishi',
+  'car_year': 1987,
+  'year_born': '1997'
+}, {
+  'id': 58,
+  'first_name': 'Alvan',
+  'last_name': 'Doring',
+  'email': 'adoring1l@mozilla.com',
+  'gender': 'Male',
+  'company name ': 'Twitterlist',
+  'car_make': 'Mazda',
+  'car_year': 1995,
+  'year_born': '1989'
+}, {
+  'id': 59,
+  'first_name': 'Robinia',
+  'last_name': 'Grishelyov',
+  'email': 'rgrishelyov1m@gmpg.org',
+  'gender': 'Genderfluid',
+  'company name ': 'Oyope',
+  'car_make': 'Honda',
+  'car_year': 2009,
+  'year_born': '1998'
+}, {
+  'id': 60,
+  'first_name': 'Yorgos',
+  'last_name': 'Le Provest',
+  'email': 'yleprovest1n@amazon.de',
+  'gender': 'Female',
+  'company name ': 'Jetpulse',
+  'car_make': 'Aston Martin',
+  'car_year': 2006,
+  'year_born': '1991'
+}];
